38fb95ea596147cdfce4ace091b7c1bd475fff7f
Елизавета Водолазская <elizavetavxa@gmail.com>
2025-10-26 23:30:01 +0300

    added the output of the sum of 2 numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::io;
 
+fn main(){
+	let mut input = String::new();
+	io::stdin()
+		.read_line(&mut input)
+		.expect("Couldn't read the line");
+	let numbers: Vec<i32> = input
+		.split_whitespace()
+		.map(|s| s.parse().expect("Enter the correct numbers"))
+		.collect();
+	if numbers.len() < 2 {
+		println!("You must enter at least 2 numbers");
+		return;
+	}
+	println!("{}", numbers[0]+numbers[1]);
 }
